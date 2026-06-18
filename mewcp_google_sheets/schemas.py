56912ca5c65fd703e496c3889d29de49eb5e9a2d
Ammar Shaikh <shaikhmohammedammar62@gmail.com>
2026-06-18 14:42:10 +0530

    refractoring - other, spreadsheet

--- mewcp_google_sheets/schemas.py
+++ mewcp_google_sheets/schemas.py
@@ -438,7 +438,7 @@ class DimensionRange(BaseModel):
     dimension: Dimension = Field(..., description="The dimension of the span.")
     startIndex: int = Field(
         ..., description="The start (inclusive) of the span, or not set if unbounded."
-    )
+    )DataSourceColumnReference
     endIndex: int = Field(
         ..., description="The end (exclusive) of the span, or not set if unbounded."
     )
@@ -472,7 +472,7 @@ class DimensionProperties(BaseModel):
     hiddenByUser: bool
     pixelSize: int
     developerMetadata: List[DeveloperMetadata]
-    dataSourceColumnReference: DataSourceColumnReference
+    dataSourceColumnReference:
 
 
 class DataSourceSheetDimensionRange(BaseModel):
@@ -643,8 +643,10 @@ class PivotGroupValueMetadata(BaseModel):
     )
 
 
-class SortOrder(BaseModel):
-    pass
+class SortOrder(Enum):
+    SORT_ORDER_UNSPECIFIED = "SORT_ORDER_UNSPECIFIED"
+    ASCENDING = "ASCENDING"
+    DESCENDING = "DESCENDING"
 
 
 class PivotGroupSortValueBucket(BaseModel):
@@ -806,7 +808,7 @@ class FilterCriteria(BaseModel):
     visibleForegroundColorStyle: ColorStyle
 
 class FilterSpec(BaseModel):
-    filterCriteria = FilterCriteria
+    filterCriteria: FilterCriteria
     columnIndex: int
     dataSourceColumnReference: DataSourceColumnReference
 
